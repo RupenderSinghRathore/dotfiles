91acf0b3da9d6d56da2ec16753d86bda6751e65c
kami <kamisamakirishima@gmail.com>
2025-07-28 18:06:02 +0530

    started again

--- .config/scripts/tmux-file-nvim-session.sh
+++ .config/scripts/tmux-file-nvim-session.sh
@@ -1,7 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
+##!/bin/zsh
 
 # Define the directories you want to search
-search_dirs=(Documents Downloads Pictures  dotfiles lunaar)
+search_dirs=(Documents Downloads dotfiles lunaar)
 
 # Search for files in the specified directories, optionally excluding certain subdirectories (e.g., .git)
 file=$(find "${search_dirs[@]}" -type d -name '.git' -prune -o -type f -print | fzf --prompt="Select file: ")
--- .config/scripts/tmux-fzf-new-window.sh
+++ .config/scripts/tmux-fzf-new-window.sh
@@ -1,4 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
+##!/bin/zsh
 
 search_dirs=(Documents Downloads Projects lunaar dotfiles Games)
 dir=$(find "${search_dirs[@]}" -type d -name '.git' -prune -o -type d -print 2>/dev/null | fzf --prompt="Select directory: ")
