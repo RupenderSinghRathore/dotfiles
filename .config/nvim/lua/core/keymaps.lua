894527a50c91cbf90cf7384b0289bf5cf8c13546
kami <kamisamakirishima@gmail.com>
2025-07-30 01:17:40 +0530

    cleaned some nvim plugins

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -23,14 +23,14 @@ vim.api.nvim_set_keymap("t", "jj", [[<C-\><C-n>]], opts)
 -- save file without auto-formatting
 vim.keymap.set("n", "<leader>ns", "<cmd>noautocmd w <CR>", opts)
 
-vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts)   -- close buffer
+vim.keymap.set("n", "<leader>x", ":bdelete!<CR>", opts) -- close buffer
 vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
 
 -- Tabs
 vim.keymap.set("n", "<leader>tn", ":tabnew ", opts) -- open new tab
 -- vim.keymap.set("n", "tx", ":tabclose ", opts)       -- close current tab
-vim.keymap.set("n", "tn", ":tabn<CR>", opts)        --  go to next tab
-vim.keymap.set("n", "tp", ":tabp<CR>", opts)        --  go to previous tab
+vim.keymap.set("n", "tn", ":tabn<CR>", opts) --  go to next tab
+vim.keymap.set("n", "tp", ":tabp<CR>", opts) --  go to previous tab
 
 -- Toggle line wrapping
 vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
@@ -44,3 +44,5 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- remapping u and U
 vim.keymap.set("n", "U", ":undo<CR>", { noremap = true })
 vim.keymap.set("n", "u", "", { noremap = true })
+
+vim.keymap.set("n", "<leader>ge", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -3,20 +3,20 @@ return {
     -- Hints keybinds
     "folke/which-key.nvim",
   },
-  {
-    -- Autoclose parentheses, brackets, quotes, etc.
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    opts = {},
-  },
-  {
-    -- Highlight todo, notes, etc in comments
-    "folke/todo-comments.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
-  },
+  -- {
+  --   -- Autoclose parentheses, brackets, quotes, etc.
+  --   "windwp/nvim-autopairs",
+  --   event = "InsertEnter",
+  --   config = true,
+  --   opts = {},
+  -- },
+  -- {
+  --   -- Highlight todo, notes, etc in comments
+  --   "folke/todo-comments.nvim",
+  --   event = "VimEnter",
+  --   dependencies = { "nvim-lua/plenary.nvim" },
+  --   opts = { signs = false },
+  -- },
   {
     -- High-performance color highlighter
     "norcalli/nvim-colorizer.lua",
